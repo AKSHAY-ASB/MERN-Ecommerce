2e7030de1b6a3cd16a4426eabe4e140149f00d36
Akshay <akshay28.seti@gmail.com>
2025-07-30 16:05:17 +0530

    added minor change

--- server/server.js
+++ server/server.js
@@ -25,6 +25,10 @@ const app = express();
 
 const PORT = process.env.PORT || 5000 ;
 
+app.get("/", (req, res) => {
+  res.send("Backend is working!");
+})
+
 app.use(
     cors({
         origin:process.env.CLIENT_BASE_URL,
