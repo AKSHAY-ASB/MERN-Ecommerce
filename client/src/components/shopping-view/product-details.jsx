67c8391a07d8c93732c56412cdcea796c95c612c
Akshay <akshay28.seti@gmail.com>
2025-08-05 11:49:52 +0530

    fixed product modal in responsive

--- client/src/components/shopping-view/product-details.jsx
+++ client/src/components/shopping-view/product-details.jsx
@@ -93,7 +93,7 @@ const ProductDetailDialog = ({ open, setOpen, productDetails }) => {
 
   return (
     <Dialog open={open} onOpenChange={handleDialogClose}>
-      <DialogContent className="grid grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:[max-w-[70vw]">
+      <DialogContent className="grid sm:grid-cols-2 gap-8 sm:p-12 max-w-[90vw] sm:max-w-[80vw] lg:[max-w-[70vw]">
         <div className="relative overflow-hidden rounded-lg">
           <img
             src={productDetails?.image}
